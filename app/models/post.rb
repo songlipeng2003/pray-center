826a26ceea976817f0cf5c9eee9d3a17566d6d7a
Thinking Song <123wuming_123@163.com>
2016-08-04 13:39:55 +0800

    add post speed_limit

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,14 @@ class Post < ApplicationRecord
   validates :content, presence: true
   validates :category, presence: true
   validates :region, presence: true
+
+  validate :speed_limit, on: :create
+
+  scope :today, -> { where("created_at > ?", Date.today) }
+
+  def speed_limit
+    if user.posts.today.count>2
+      errors.add(:title, '今天发帖量已经达到最大限制')
+    end
+  end
 end
