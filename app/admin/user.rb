dd613cfa0099ac087fcc962ed95d4efab592b61c
Thinking Song <123wuming_123@163.com>
2016-08-06 22:04:54 +0800

    fix admin create user error

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :phone, :password, :password_confirmation
+  permit_params :username, :phone, :password, :password_confirmation
 
   index do
     selectable_column
