c00964ebd4f3f5340c2c66e1682df0112fb1e493
Thinking Song <123wuming_123@163.com>
2016-09-19 22:04:03 +0800

    add prays receive api

--- app/api/v1/prays.rb
+++ app/api/v1/prays.rb
@@ -5,7 +5,7 @@ module V1
     end
 
     resource :prays do
-      desc '代祷列表',
+      desc '用户发出代祷列表',
         is_array: true,
         http_codes: [
           [201, '成功', V1::Entities::PrayHistory],
@@ -19,6 +19,25 @@ module V1
       end
       get do
         result = paginate current_user.pray_histories
+
+        present result, with: V1::Entities::PrayHistory
+      end
+
+      desc '用户收到的代祷列表',
+        is_array: true,
+        http_codes: [
+          [201, '成功', V1::Entities::PrayHistory],
+          [401, '未授权', V1::Entities::Error],
+        ]
+      paginate per_page: 10, max_per_page: 200
+      params do
+        optional 'X-Access-Token', type: String, desc: 'Token', documentation: { in: :header }
+        optional :page, type: Integer, default: 1, desc: "页码"
+        optional :per_page, type: Integer, default: 10, desc: '每页数量'
+      end
+      get :receive do
+        query = PrayHistory.joins(:post).where(posts: { user_id: current_user.id })
+        result = paginate query
 
         present result, with: V1::Entities::PrayHistory
       end
@@ -67,7 +86,12 @@ module V1
         put :thank do
           check_user_info!
 
-          pray_history = current_user.pray_histories.find(params[:history_id])
+          pray_history = PrayHistory.find(params[:history_id])
+
+          if pray_history.post.user!=current_user
+            error!('Not Found', 404)
+          end
+
           if pray_history.is_thanked
             error!('已经感谢代祷，不能重复感谢代祷', 422)
           end
@@ -91,7 +115,7 @@ module V1
       post :batch_thank do
         check_user_info!
 
-        current_user.pray_histories.update_all is_thanked: true
+        PrayHistory.joins(:post).where(posts: { user_id: current_user.id }).update_all is_thanked: true
 
         { result: true }
       end
